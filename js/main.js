d4ac90a5d54987828b875e72f8e7d2f681c28cf8
genghaot <teogenghao@gmail.com>
2019-03-26 16:31:45 +0800

    Website updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
-$(document).ready(function(){
+jQuery(document).ready(function($){
+    var isLateralNavAnimating = false;
+    
+    //open/close lateral navigation
+    $('.cd-nav-trigger').on('click', function(event){
+        event.preventDefault();
+        //stop if nav animation is running 
+        if( !isLateralNavAnimating ) {
+            if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+            
+            $('body').toggleClass('navigation-is-open');
+            $('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+                //animation is over
+                isLateralNavAnimating = false;
+            });
+        }
+    });
+
+    $('.cd-primary-nav-link').on('click', function(event){
+        event.preventDefault();
+        //stop if nav animation is running 
+        if( !isLateralNavAnimating ) {
+            if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+            
+            $('body').toggleClass('navigation-is-open');
+            $('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+                //animation is over
+                isLateralNavAnimating = false;
+            });
+        }
+    });
+
     var mn = $(".navbar");
     mns = "scrolled";
     hdr = 24;
